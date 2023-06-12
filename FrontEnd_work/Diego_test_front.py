08c6e462c4242c2e07e127ed9b59f38a1c5fe099
Egerishshe <avstrovengria257@gmail.com>
2023-06-13 09:25:51 +1000

    Изменения таковы: создана крутая scroll таблица и переменные, которые содержат то, что ввел пользователь, таблица значений

--- FrontEnd_work/Diego_test_front.py
+++ FrontEnd_work/Diego_test_front.py
@@ -1,24 +1,27 @@
 from tkinter import *
 from tkinter import ttk
+
+
 def codify():
-    sentence = t.get(1.0,END)
+    sentence = t.get(1.0, END)
     t_out.delete(1.0, END)
     t_out.insert(1.0, sentence)
 
+
 def data_t():
-    sentence = t.get(1.0,END)
+    sentence = t.get(1.0, END)
     data = list(sentence)
     count = 0
+    for record in table.get_children():
+        table.delete(record)
     for record in data:
-        table.insert(parent='', index=END, iid=count, text='', values=(record[0]))
+        table.insert(parent='', index=END, text='', values=(record[0]))
         count += 1
     table.grid(row=10, column=0)
 
 
 win = Tk()
 win.title("Кодировщик по методу Шенона фано")
-# win.iconbitmap()
-# photo = PhotoImage(file=)
 
 t = Text(win, width=20, height=6)
 t.grid(column=0, row=1, sticky='we')
@@ -34,7 +37,7 @@ lbl_out = Label(text='Выход').grid(row=0, column=3, sticky='we')
 
 table = ttk.Treeview(win, columns=('first', 'second'), show='headings')
 table.heading('first', text='Буквы')
-table.heading('second', text='Симболы')
+table.heading('second', text='Символы')
 
 win.grid_columnconfigure(0, minsize=200)
 win.grid_columnconfigure(1, minsize=100)
