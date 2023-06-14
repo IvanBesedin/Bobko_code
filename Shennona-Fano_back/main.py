00cf319f9630df38c8ce986e3d2f6a3b9198b341
Santy <santilopez2002@hotmail.com>
2023-06-14 14:25:29 +1000

    Код для определения того, какой ключ имеет наибольшую длину,определить ошибку

--- Shennona-Fano_back/main.py
+++ Shennona-Fano_back/main.py
@@ -1,6 +1,8 @@
 from shennonFanoCompression import shennon_fano_compression
 from shennonFanoDecompression import shennon_fano_decompression
+from max_value import mayor, lengths
 
+print(mayor(lengths))
 result = shennon_fano_compression()
 print(result.code)
 print(result.dictionary)
