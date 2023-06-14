80092839f21c81134bc1579b03011b8ecabd720a
Santy <santilopez2002@hotmail.com>
2023-06-14 14:40:23 +1000

    Revert "Код для определения того, какой ключ имеет наибольшую длину,определить ошибку"
    This reverts commit 00cf319f9630df38c8ce986e3d2f6a3b9198b341.

--- Shennona-Fano_back/main.py
+++ Shennona-Fano_back/main.py
@@ -1,8 +1,6 @@
 from shennonFanoCompression import shennon_fano_compression
 from shennonFanoDecompression import shennon_fano_decompression
-from max_value import mayor, lengths
 
-print(mayor(lengths))
 result = shennon_fano_compression()
 print(result.code)
 print(result.dictionary)
--- Shennona-Fano_back/shennonFanoDecompression.py
+++ Shennona-Fano_back/shennonFanoDecompression.py
@@ -1,27 +1,15 @@
-from max_value import lengths, mayor
 def shennon_fano_decompression(code, dict_binary_to_letters):
     """Decompresses a string code of 0 -s and 1 -s, using a dictionary, and returns a letter string.
 
     Takes as a parameter a string code of 0 -s and 1 -s, and a dictionary that has a structure {Binary code : Letter}
     """
-
     message = ""
     symbol = ""
-
     for n in code:
         symbol += n
         attempt = dict_binary_to_letters.get(symbol, False)
-
         if attempt:
             message += attempt
             symbol = ""
-        print(symbol,attempt, len(symbol))
 
-        if len(symbol) == mayor(lengths):
-            break
-
-    if len(symbol) == mayor(lengths):
-        return "Есть ошибка."
-    else:
-        return message
-
+    return message
